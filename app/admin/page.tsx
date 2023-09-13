7a050772dd576eb1ac0db70558de6d9445ae0874
Jin Young Bang <jybang@bu.edu>
2023-09-12 23:12:34 -0400

    feat: link API for get all listings

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,22 +1,34 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ListingCard from "@/components/admin/ListingCard";
 
+interface Listing {
+    title: string;
+    active: boolean;
+    dateCreated: string; // You can use a more specific date type if needed
+    // Define other properties as per your API response
+  }
+  
 
 export default function Admin() {
 
-    const cardsData = [
-        { title: "Fall 2023", active: true, dateCreated: "09/01/23", applicantCount: 142 },
-        { title: "Spring 2023", active: false, dateCreated: "01/13/23", applicantCount: 294 },
-        { title: "Fall 2022", active: false, dateCreated: "09/12/22", applicantCount: 23 },
-    ];
+    const [listings, setListings] = useState<Listing[]>([]);
+
+
+    useEffect(() => {
+        // Fetch listings data from your /listings API endpoint
+        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/listings`)
+            .then((response) => response.json())
+            .then((data: Listing[]) => setListings(data))
+            .catch((error) => console.error("Error fetching listings:", error));
+    }, []);
 
     return (
         <main className="container mx-auto p-8">
             <div className="grid grid-cols-3 gap-4">
-                {cardsData.map((card, index) => (
+                {listings.map((listing, index) => (
                     <div key={index} className="col-span-1">
-                        <ListingCard title={card.title} active={card.active} dateCreated={card.dateCreated} applicantCount={card.applicantCount}/>
+                        <ListingCard title={listing.title} active={true} dateCreated={listing.dateCreated} applicantCount={0} />
                     </div>
                 ))}
             </div>
