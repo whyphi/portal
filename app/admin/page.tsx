d2a6055cf52c66612d732c5220d20dfc82d47af0
Jin Young Bang <jybang@bu.edu>
2023-09-14 12:15:27 -0400

    fix: implement listing card wrap around

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,38 +1,42 @@
-'use client'
+"use client"
 import { useEffect, useState } from "react";
 import ListingCard from "@/components/admin/ListingCard";
 
 interface Listing {
-    listingId: string;
-    title: string;
-    active: boolean;
-    deadline: string;
-    dateCreated: string;
-  }
-  
+  listingId: string;
+  title: string;
+  active: boolean;
+  deadline: string;
+  dateCreated: string;
+}
 
 export default function Admin() {
+  const [listings, setListings] = useState<Listing[]>([]);
 
-    const [listings, setListings] = useState<Listing[]>([]);
+  useEffect(() => {
+    // Fetch listings data from your /listings API endpoint
+    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/listings`)
+      .then((response) => response.json())
+      .then((data: Listing[]) => setListings(data))
+      .catch((error) => console.error("Error fetching listings:", error));
+  }, []);
 
-
-    useEffect(() => {
-        // Fetch listings data from your /listings API endpoint
-        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/listings`)
-            .then((response) => response.json())
-            .then((data: Listing[]) => setListings(data))
-            .catch((error) => console.error("Error fetching listings:", error));
-    }, []);
-
-    return (
-        <main className="container mx-auto p-8">
-            <div className="grid grid-cols-3 gap-4">
-                {listings.map((listing, index) => (
-                    <div key={index} className="col-span-1">
-                        <ListingCard listingId={listing.listingId} title={listing.title} active={true} deadline={listing.deadline} dateCreated={listing.dateCreated} applicantCount={0} />
-                    </div>
-                ))}
-            </div>
-        </main>
-    );
+  return (
+    <main className="container mx-auto p-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {listings.map((listing, index) => (
+          <div key={index} className="col-span-1">
+            <ListingCard
+              listingId={listing.listingId}
+              title={listing.title}
+              active={true}
+              deadline={listing.deadline}
+              dateCreated={listing.dateCreated}
+              applicantCount={0}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
