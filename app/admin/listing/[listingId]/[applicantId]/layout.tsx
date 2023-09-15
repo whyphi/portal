4e987b36365c167a89191b35c02f259399b0b91d
Jin Young Bang <jybang@bu.edu>
2023-09-15 13:31:08 -0400

    chore: add layout to applicant view

---
+++ app/admin/listing/[listingId]/[applicantId]/layout.tsx
@@ -0,0 +1,22 @@
+import '../../../../globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Whyphi - Create',
+    description: "Whyphi's Admin",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+
+        <div className="flex flex-col mx-auto justify-center max-w-screen-lg w-full">
+            {children}
+        </div>
+
+
+    )
+}
