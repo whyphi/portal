3e68db8d14797d5aaf69ec320592a348e31226da
Jin Young Bang <jybang@bu.edu>
2023-09-16 15:56:41 -0400

    chore: fix typescript

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -1,38 +1,35 @@
 "use client"
-import React, { useState, useEffect } from "react";
+
+import React from "react";
 import Form from "@/components/Form";
 import Loader from "@/components/Loader";
 import { useRouter } from "next/navigation";
 import useSWR from 'swr';
 
-interface Listing {
-  deadline: string;
-  listingId: string;
-  questions: [] | [{ question: string; context: string }];
+interface ListingData {
   title: string;
+  questions: [{ question: string; context: string }];
+  listingId: string;
+  deadline: string;
 }
 
-
 export default function Listing({ params }: { params: { id: string } }) {
-  const router = useRouter()
+  const router = useRouter();
 
-  const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_API_BASE_URL}/listings/${params.id}`, fetcher)
+  const fetcher = (...args: Parameters<typeof fetch>) =>
+    fetch(...args).then(res => res.json());
+  const { data, error, isValidating } = useSWR<ListingData>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/listings/${params.id}`,
+    fetcher
+  );
 
-  if (error) router.push("/error")
+  if (error) router.push("/error");
 
-  // const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [listingData, setListingData] = useState<Listing>({
-    deadline: "",
-    listingId: "",
-    questions: [],
-    title: "",
-  });
+  if (!data || isValidating) {
+    return <Loader />;
+  }
 
-
-  return isLoading ? (
-    <Loader />
-  ) : (
+  return (
     <main className="flex flex-col mx-auto justify-center items-center max-w-screen-sm px-5 py-2.5">
       <Form
         title={data.title}
