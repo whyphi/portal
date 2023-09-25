b51c6bb9ce3aaa5a5e99e48f5260ca7340917d4d
Jin Young Bang <jybang@bu.edu>
2023-09-24 22:08:10 -0400

    fix: Fix for code review

--- components/admin/NavSidebar.tsx
+++ components/admin/NavSidebar.tsx
@@ -66,7 +66,7 @@ export default function NavSidebar() {
                     Sign Out
                   </button>
                 </Dropdown>
-              ) : ("nope bye")}
+              ) : ("")}
 
             </div>
           </div>
