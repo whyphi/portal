f31160c4f34e63f9af2901d600b283f8cc970e68
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-01-09 13:02:15 -0500

    dropdown appears funcitonal (but parsing data has errors)

--- app/admin/listing/[listingId]/insights/page.tsx
+++ app/admin/listing/[listingId]/insights/page.tsx
@@ -4,11 +4,14 @@ import Loader from "@/components/Loader";
 import ApplicantCard from "@/components/admin/listing/ApplicantCard";
 import { DistributionMetricsState, Metrics } from "@/types/insights"
 import { Applicant } from "@/types/applicant";
-import { ResponsiveContainer, PieChart, Pie, Tooltip } from "recharts";
+import { PieChart, Pie, Tooltip, Label } from "recharts";
+import { Dropdown } from 'flowbite-react';
 
 
-export default function test({ params }: { params: { listingId: string } }) {
+export default function Insights({ params }: { params: { listingId: string } }) {
+  // applicantData : list of applicants
   const [applicantData, setApplicantData] = useState<[] | [Applicant]>([]);
+  // distributionMetrics : object containing frequencies of each metric for all applicants
   const [distributionMetrics, setDistributionMetrics] = useState<DistributionMetricsState>({
     colleges: [],
     gpa: [],
@@ -18,12 +21,18 @@ export default function test({ params }: { params: { listingId: string } }) {
     linkedin: [],
     website: [],
   });
+
+  // fields : list of all fields being used for analytics
   const fields : string[] = ["colleges", "gpa", "gradYear", "major", "minor", "linkedin", "website"]
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  // selectedItem : used to track which metric plot pie chart for
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+  
+  
   // Fetch listings data from your /listings API endpoint
   useEffect(() => {
-    
     fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/applicants/${params.listingId}`)
     .then((response) => response.json())
     .then((data: [Applicant]) => {
@@ -33,6 +42,8 @@ export default function test({ params }: { params: { listingId: string } }) {
     .catch((error) => console.error("Error fetching applicants:", error));
     
   }, [])
+  
+  // parse through data every time applicantData finishes loading
   useEffect(() => {
     try {
       if (applicantData) {
@@ -42,34 +53,31 @@ export default function test({ params }: { params: { listingId: string } }) {
       console.log("error parsing data:", error)
     }
   }, [applicantData])
-
-  console.log(distributionMetrics)
-  console.log(distributionMetrics.major[0]?.value)
-
+  
   // parseData : iterates over list of applicantData and obtains distribution of metrics (colleges, gpa, gradYear, major, minor, linkedin, website)
   const parseData = () => {
     // create temporary copy of distributionMetrics (update after)
     const updatedMetrics = { ...distributionMetrics };
-
+    
     // map through list of applicants
     applicantData.map((applicant: Applicant) => {
       // iterate through keys of applicant object (only consider valid ones)
       Object.entries(applicant).forEach(([metric, val]: [string, string]) => {
         // valOut : changes to boolean if metric is "linkedin/website" --> otherwise string metric
         let valOut: string | boolean = val
-
+        
         if (!fields.includes(metric)) {
           // case 1: ignore irrelevant metrics
           return;
           
         } else if (metric == "colleges") {
           // case 2: if colleges -> iterate over list of colleges
-
+          
           return;
-
+          
         } else if (["linkedin", "website"].includes(metric)) {
-            // case 3: hasUrl -> true or false depending on if user has linkedin/website
-            valOut = val.includes("https://www.");
+          // case 3: hasUrl -> true or false depending on if user has linkedin/website
+          valOut = val.includes("https://www.");
         } 
         // handle all other metric updates
         const foundMetric = updatedMetrics[metric].find(metricObject => metricObject?.name === valOut);
@@ -79,21 +87,39 @@ export default function test({ params }: { params: { listingId: string } }) {
           const newMetric: Metrics = { name: valOut, value: 1};
           updatedMetrics[metric].push(newMetric);
         }
-          
+        
       })
     })
     setDistributionMetrics(updatedMetrics)
   }
+  
+  // handleDropdownChange : updates title of dropdown...
+  const handleDropdownChange = (selectedItem: string) => {
+    setSelectedItem(selectedItem);
+  };
+  console.log("metric", selectedItem);
 
   // if applicants data not yet received : produce loading screen
   if (isLoading) return (<Loader />)
 
- 
   return (
     <div>
-        <PieChart width={730} height={250}>
-          {distributionMetrics.major.length > 0 ? 
-            <Pie data={distributionMetrics.gradYear} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={80} outerRadius={100} fill="#82ca9d" label />
+        <Dropdown label={selectedItem || 'Select a metric'}>
+          {fields.map((field, index) => (
+            <Dropdown.Item key={index} onClick={() => handleDropdownChange(field)}>
+              {field}
+            </Dropdown.Item>
+          ))}
+        </Dropdown>
+        <PieChart width={500} height={400}>
+          {selectedItem && distributionMetrics[selectedItem].length > 0 ? 
+            <Pie data={distributionMetrics[selectedItem]} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={130} outerRadius={160} fill="#82ca9d" label > 
+              {/* {distributionMetrics.gradYear.map((entry, index) => (
+                <Label key={`label-${index}`} position="center">
+                  {entry.name}
+                </Label>
+              ))} */}
+            </Pie>
             :
             <Loader />
           }
