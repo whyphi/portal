7342d63e4a4c6df8375433fe84784db1afda3e8f
Taesung Yoon <yoontsy2000@gmail.com>
2024-01-10 13:49:14 +0900

    Just noticed we are not using Navbarlol

--- components/admin/NavSidebar.tsx
+++ components/admin/NavSidebar.tsx
@@ -14,7 +14,6 @@ export default function NavSidebar() {
 
   return (
     <>
-
       <nav className={`fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700 ${isSidebarOpen ? 'lg:pl-64' : 'lg:px-5'}`}>
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
