a189270761541ab99607c1f4a9d56f134a3ce7e7
Taesung Yoon <yoontsy2000@gmail.com>
2024-01-10 13:39:45 +0900

    Removed fixed number margin and padding in admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,29 +4,22 @@ import NavSidebar from "@/components/admin/NavSidebar";
 import { useSession, signIn } from "next-auth/react"
 import Loader from '@/components/Loader';
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode,
-}) {
-  const { data: session, status } = useSession()
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const { data: session, status } = useSession();
 
   if (status === "loading") {
     return <Loader />
-  }
+  };
 
   if (status === "unauthenticated") {
     signIn('google')
     return
-  }
+  };
 
   return (
     <>
       <NavSidebar />
-      <div className="p-8 sm:ml-64 mt-14">
-        {children}
-      </div>
+      {children}
     </>
-
-  )
+  );
 }
