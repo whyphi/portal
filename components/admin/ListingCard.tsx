40a0590ff160f53ba2d52c87a54a2171240fc7a1
Taesung Yoon <yoontsy2000@gmail.com>
2024-01-23 12:21:04 -0500

    Added background and pushed settings to top window

--- components/admin/ListingCard.tsx
+++ components/admin/ListingCard.tsx
@@ -102,6 +102,8 @@ export default function ListingCard({ listingId, title, active, deadline, dateCr
   };
 
   const handleToggleSwitchChange = async (isChecked: boolean) => {
+    
+
     try {
       // Update the local state
       setIsActive(isChecked);
@@ -144,21 +146,10 @@ export default function ListingCard({ listingId, title, active, deadline, dateCr
 
 
   return (
-    <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Link href={`admin/listing/${listingId}`} className="p-6">
+    <div className="min-h-full flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-between items-center py-2 px-6 bg-violet-100 rounded-t-lg">
+        {renderIndicator(isActive)}
         <div className="flex justify-between items-center">
-          {renderIndicator(isActive)}
-          <ToggleSwitch className="" checked={isActive} label="" onChange={handleToggleSwitchChange} onClick={handleToggleSwitchClick} />
-        </div>
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        <div className="mt-1">
-          {renderDateCreatedBadge(dateCreated)}
-        </div>
-      </Link>
-      <div className="px-6 pb-6">
-        <hr className="h-px mb-2 mt-4 bg-gray-200 border-0 dark:bg-gray-700" />
-        <div className="flex justify-between items-center">
-          {renderDeadline(deadline)}
           <div className="dropdown-container cursor-pointer"> {/* Add a class to identify the dropdown */}
             <Dropdown label="" dismissOnClick={false} renderTrigger={() => <span>{renderSettingsIcon()}</span>}>
               <Dropdown.Item href={`public/${listingId}`}>View Public Listing</Dropdown.Item>
@@ -167,7 +158,14 @@ export default function ListingCard({ listingId, title, active, deadline, dateCr
           </div>
         </div>
       </div>
+      <Link href={`admin/listing/${listingId}`} className="py-6 px-6 min-h-full flex-grow">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+        <div className="mt-1">
+          {renderDateCreatedBadge(dateCreated)}
+        </div>
+        <hr className="h-px mb-2 mt-4 bg-gray-200 border-0 dark:bg-gray-700" />
+        {renderDeadline(deadline)}
+      </Link>
     </div >
   );
-
 }
