03cc3716557ff77433e12a5b492928f4bf3756c1
Jin Young Bang <jybang@bu.edu>
2024-04-27 10:13:33 -0400

    fix: lint

--- app/admin/accountability/page.tsx
+++ app/admin/accountability/page.tsx
@@ -129,7 +129,7 @@ export default function Accountability() {
 
               <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
 
-              <p className="mt-4 text-gray-500">Something broke. We'll try find a fix soon!</p>
+              <p className="mt-4 text-gray-500">{`Something broke. We'll try find a fix soon!`}</p>
             </div>
           </div>
         )}
