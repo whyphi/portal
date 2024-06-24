0b5877bd50e9be6ef8a2710cb771ef0eedd3d93b
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-06-24 18:11:21 -0400

    fixed date.ts merge errors

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,7 @@
 export const formatMongoDate = (dateString: string): string => {
     const date = new Date(dateString);
+
+    const year = date.getFullYear();
     const month = `0${date.getMonth() + 1}`.slice(-2);
     const day = `0${date.getDate()}`.slice(-2);
 
@@ -12,5 +14,3 @@ export const formatMongoDate = (dateString: string): string => {
 
     return `${year}.${month}.${day} (${hours}:${minutes} ${ampm})`;
 };
-
-
