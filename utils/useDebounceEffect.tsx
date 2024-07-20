5e958681445fd1273bd2bf4b6c13776f5108f8c1
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-20 15:39:58 -0400

    added function description

--- utils/useDebounceEffect.tsx
+++ utils/useDebounceEffect.tsx
@@ -1,5 +1,8 @@
 import { useEffect, DependencyList } from "react"
 
+// useDebounceEffect : custom React hook that debounces the execution of a given function
+// (used for the CropImage component to ensure the completed crop is only executed every 
+// `waitTime` ms)
 export function useDebounceEffect(
   fn: () => void,
   waitTime: number,
