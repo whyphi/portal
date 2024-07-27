97e70f6b856d5b8a9ee196ae0d270f9e2be23b92
Jin Young Bang <jybang@bu.edu>
2024-07-27 14:27:08 -0400

    feat: create Member interface for account settings

---
+++ types/admin/account-settings/member.ts
@@ -0,0 +1,15 @@
+interface Member {
+  _id: string;
+  name: string;
+  email: string;
+  class: string;
+  college: string;
+  family: string;
+  graduationYear: string;
+  isEboard: string;
+  major: string;
+  minor: string;
+  isNewUser: boolean;
+  team: string;
+  roles: string[];
+}
