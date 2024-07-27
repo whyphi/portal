9ad26a893c5c49a2f621261369cf813d736978e3
Jin Young Bang <jybang@bu.edu>
2024-07-27 14:30:49 -0400

    refactor: changed some member fields to optional

--- types/admin/account-settings/member.ts
+++ types/admin/account-settings/member.ts
@@ -2,14 +2,18 @@ interface Member {
   _id: string;
   name: string;
   email: string;
-  class: string;
-  college: string;
-  family: string;
-  graduationYear: string;
-  isEboard: string;
-  major: string;
-  minor: string;
-  isNewUser: boolean;
-  team: string;
   roles: string[];
+
+  // Optional fields due to some members being not onboarded yet
+  class?: string;
+  college?: string;
+  family?: string;
+  graduationYear?: string;
+  isEboard?: string;
+  major?: string;
+  minor?: string;
+  isNewUser?: boolean;
+  team?: string;
+  big?: string;
+  littles?: string[];
 }
