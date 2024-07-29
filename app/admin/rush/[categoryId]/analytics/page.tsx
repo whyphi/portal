d28ad2632912f977eeb207950d06d01cbfc9ef3a
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-28 20:26:08 -0400

    added Drawer (needs some work in separate PR)

--- app/admin/rush/[categoryId]/analytics/page.tsx
+++ app/admin/rush/[categoryId]/analytics/page.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "@/app/contexts/AuthContext";
 import Loader from "@/components/AdminLoader";
 import { Analytics } from "@/types/admin/events";
-import { Table } from "flowbite-react";
+import { Drawer, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
 
 export default function RushAnalytics({ params }: { params: { categoryId: string } }) {
@@ -11,6 +11,12 @@ export default function RushAnalytics({ params }: { params: { categoryId: string
   const [isLoading, setIsLoading] = useState(true);
   const [analytics, setAnalytics] = useState<Analytics>({});
   const [error, setError] = useState<Error>();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => setIsOpen(false);
 
   useEffect(() => {
     const fetchAnalytics = async () => {
@@ -49,7 +55,7 @@ export default function RushAnalytics({ params }: { params: { categoryId: string
         {/* TODO: create function to determine if the candidate can be accepted for interview */}
         <Table.Cell>{analytics[email].eventsAttended.length > 4 ? "True" : "False"}</Table.Cell>
         <Table.Cell>
-          <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+          <a onClick={handleOpen} className="font-medium text-cyan-600 hover:underline hover:cursor-pointer dark:text-cyan-500">
             Details
           </a>
         </Table.Cell>
@@ -69,13 +75,23 @@ export default function RushAnalytics({ params }: { params: { categoryId: string
           <Table.HeadCell>Events Attended</Table.HeadCell>
           <Table.HeadCell>Threshold</Table.HeadCell>
           <Table.HeadCell>
-            <span className="sr-only">Edit</span>
+            <span className="sr-only">Details</span>
           </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
           {renderAnalyticsTable()}
         </Table.Body>
       </Table>
+
+      <Drawer className="z-50" open={isOpen} onClose={handleClose} position="right">
+        <Drawer.Header title="Drawer" />
+        <Drawer.Items>
+          <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
+            In progress...
+          </p>
+          
+        </Drawer.Items>
+      </Drawer>
     </div>
   )
 }
