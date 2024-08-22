8888edb4bd013156c2aa706743c0e0beabae1849
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-08-22 15:42:06 -0400

    more temporary debugging on baseUrl

--- utils/getBaseURL.ts
+++ utils/getBaseURL.ts
@@ -1,14 +1,15 @@
 // getRushBaseUrl : returns base url of rush application (depending on current environment)
 export const getRushBaseUrl = (): string => {
-  console.log("api base url: ", process.env.NEXT_PUBLIC_API_BASE_URL);
-  
-  switch(process.env.NEXT_PUBLIC_API_BASE_URL) {
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL?.trim();
+  console.log("api base url: ", baseUrl);
+  switch(baseUrl) {
     // local environment
     case "http://127.0.0.1:8000":
       return "http://localhost:3001";
 
     // staging environment
     case "https://o74hteay05.execute-api.us-east-1.amazonaws.com/api":
+      console.log("made it here, yay!");
       return "https://staging--whyphi-rush.netlify.app";
 
     // production environment
