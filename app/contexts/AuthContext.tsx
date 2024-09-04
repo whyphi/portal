7e421f4dec2daad38cf6a4c7bb43897521d5d7f0
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-09-04 10:22:32 -0400

    removed log

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -35,8 +35,6 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
       if (session) {
         // Use type assertion to add the 'token' property
         const sessionWithToken = session as CustomSession;
-
-        console.log("session", sessionWithToken, sessionWithToken.token?.isNewUser);
         
         // Check if user is a newUser
         if (sessionWithToken && sessionWithToken.token?.isNewUser === undefined || sessionWithToken.token?.isNewUser) {
