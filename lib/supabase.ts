cb8c809fec664309d652d48c721883ac5696e631
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-08-25 20:04:51 -0400

    fixed supabase key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -15,6 +15,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 );
